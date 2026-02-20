ef486043d1b5d61ae56da777eeb2c5731f5b78b1
Just-Prog <JustProg5835@outlook.com>
2026-02-20 22:57:00 +0800

    feat(main): nav tweaks

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,22 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
 import React from "react";
 
 export default function TabLayout() {
   return (
-    <Tabs>
+    <Tabs screenOptions={{ tabBarActiveTintColor: "dodgerblue" }}>
       <Tabs.Screen
         name="index"
         options={{
           headerShown: false,
           title: "首页",
+          tabBarIcon: (props) => (
+            <Ionicons
+              name="home-outline"
+              size={24}
+              color={props.focused ? "dodgerblue" : "gray"}
+            />
+          ),
         }}
       />
       <Tabs.Screen
@@ -16,6 +24,13 @@ export default function TabLayout() {
         options={{
           headerShown: false,
           title: "记录",
+          tabBarIcon: (props) => (
+            <Ionicons
+              name="bar-chart-outline"
+              size={24}
+              color={props.focused ? "dodgerblue" : "gray"}
+            />
+          ),
         }}
       />
       <Tabs.Screen
@@ -23,6 +38,13 @@ export default function TabLayout() {
         options={{
           headerShown: false,
           title: "我的",
+          tabBarIcon: (props) => (
+            <Ionicons
+              name="person-circle-outline"
+              size={24}
+              color={props.focused ? "dodgerblue" : "gray"}
+            />
+          ),
         }}
       />
     </Tabs>
---
+++ components/constants/color.tsx
@@ -0,0 +1,2 @@
+export const EndFieldMainColor = "rgb(255, 250, 0)";
+export const EndFieldTabColor = "rgb(255, 250, 0)";
